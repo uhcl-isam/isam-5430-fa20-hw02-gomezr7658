0e3d3d1e9a9989ba827a80f9099642f6c2c89c03
Roberto Gomez <rob.77@live.com>
2020-09-12 12:02:28 -0500

    CigarParty 001

--- CSharp.Assignments.Selection1/Logic1.cs
+++ CSharp.Assignments.Selection1/Logic1.cs
@@ -16,6 +16,15 @@ namespace CSharp.Assignments.Loop1
         /// <returns></returns>
         public static bool CigarParty(int cigars, bool isWeekend)
         {
+         if (cigars >= 40 && cigars <= 60)
+            {
+            return true;
+            }
+         if (isWeekend && cigars >= 40)
+            {
+            return true;
+            }
+         return false;
             throw new NotImplementedException();
         }
 
